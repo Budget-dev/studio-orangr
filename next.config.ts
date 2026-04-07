f8c1d242ca9ed8dd57d84e524cb8fb543e51065c
Personalstorage <personalstoragevenky@gmail.com>
2026-04-07 11:24:53 +0000

    https://vennky.sirv.com/ChatGPT%20Image%20Apr%207%2C%202026%2C%2004_47_1

--- next.config.ts
+++ next.config.ts
@@ -40,6 +40,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'vennky.sirv.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   experimental: {
